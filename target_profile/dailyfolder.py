ef0ec2148935b57cccd61b09a9d82b9ddd8d1990
Michel Albert <michel@albert.lu>
2010-11-14 13:40:57 +0100

    Deleting old files
       - The retention can be specified in the "retention" config value

--- target_profile/dailyfolder.py
+++ target_profile/dailyfolder.py
@@ -7,8 +7,21 @@ Configuration
 
 The following fields are used by this plugin:
 
-   **path**
+   **path** (string)
       The target folder
+
+   **retention** (dict) *optional*
+      How long the data should be kept. Everything older than this will be
+      deleted. The dictionary values will be passed as keyword arguments to
+      `datetime.timedelta
+      <http://docs.python.org/library/datetime.html#datetime.timedelta>`_. If
+      set to ``None``, the data will be kept indefinitely!
+
+      **Default:** ``None``
+
+      .. note:: This script uses the OSs ``mtime`` value to determine the
+                folder's date. Refer to you OS reference to see if this is what
+                you want!
 
 Configuration Example
 ~~~~~~~~~~~~~~~~~~~~~
@@ -20,14 +33,20 @@ Configuration Example
       profile = "folder",
       config = dict(
          path = "/var/backups",
+         retention = dict(
+               days=30,
+               hours=12
+            )
          ),
       ),
 
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from os.path import exists, join
-from shutil import copytree
+from os import listdir, stat
+from shutil import copytree, rmtree
+import stat as stat_info
 import logging
 import os
 
@@ -41,10 +60,32 @@ def init(target):
    TARGET.update(target)
    LOG.debug("Initialised '%s' with %r" % ( __name__, CONFIG))
 
+def remove_old_files(root, timedelta_params):
+   delta = timedelta(**timedelta_params)
+   threshold_date = datetime.now() - delta
+   LOG.info("Removing files created before %s" % threshold_date)
+   for entry in listdir(root):
+      file_meta = stat(join(root, entry))
+      mtime = datetime.fromtimestamp(file_meta[stat_info.ST_MTIME])
+      LOG.debug("Inspecting %s (mtime=%s, threshold=%s, todelete=%s)" % (
+         entry, mtime, threshold_date, mtime<threshold_date ))
+      if mtime < threshold_date:
+         LOG.info("Deleting %s" % entry)
+         rmtree(join(root, entry))
+   else:
+      LOG.info("All obsolete files successfully removed.")
+
 def run(staging_area):
    if not exists(CONFIG['path']):
       os.makedirs(CONFIG['path'])
       LOG.info("Path '%s' created." % CONFIG['path'])
+
+   # delete old files
+   timedelta_params = CONFIG.get('retention', None)
+   if timedelta_params:
+      remove_old_files(CONFIG['path'], timedelta_params)
+
+   # store new files
    now = datetime.now()
    copytree(staging_area, join(CONFIG['path'], now.strftime('%Y-%m-%d')))
 
